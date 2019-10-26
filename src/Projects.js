68312b48e4c730f194e4bf28c8787a6e5edcbf2e
faisalqureshi <faisalqureshi690@gmail.com>
2019-10-26 15:19:53 +0500

    Updated Project.js File

--- src/Projects.js
+++ src/Projects.js
@@ -1,82 +1,43 @@
 import React from 'react'
 import {Layout} from 'react-mdl'
 import Fade from 'react-reveal/Fade';
-import { makeStyles } from '@material-ui/core/styles';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {MDBCardBody,MDBCard,MDBCardTitle, MDBCardText, MDBCardFooter} from 'mdbreact'
 
 
-const useStyles = makeStyles(theme => ({
-    root: {
-      width: '30%',
-      position:'relative',
-      bottom:'-150px',
-      left:'350px'
-    }, 
-    heading: {
-      fontSize: theme.typography.pxToRem(15),
-      fontWeight: theme.typography.fontWeightRegular,
-    },
-  }));
 function Projects(){
     
     
-        const classes = useStyles();
+        
         
         return(
             <div style={{height: '132%', position: 'relative' ,bottom:'65px'}}>
             <Layout style={{background: 'url(https://previews.123rf.com/images/shutter2u/shutter2u1802/shutter2u180200410/96330479-blue-technology-circle-and-computer-science-abstract-background-with-blue-and-binary-code-matrix-bus.jpg) center / cover'}}>
-            <div className={classes.root}>
-        <Fade>
-        <ExpansionPanel>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography style={{fontSize:'20px', color:'green'}} className={classes.heading}><b><a className="a-1" href="https://github.com/FaisalQureshi200/ReactProject"  target="_blank"> React To-Do App</a></b></Typography>
-         
-        </ExpansionPanelSummary>
-        <hr/>
-        <ExpansionPanelDetails>
-          <Typography>
-            <ul>
-                <li><b>Front End App</b></li>
-                <li><b>Add and Delete Todo</b>  </li>
-                <li><b>Check Active Item and Complete Item</b></li>
-            </ul>
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
+            <Fade>
+            <MDBCard className="project-card1">
+              <MDBCardBody>
+                <img className="project1-img" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt="react"/>
+              
+              <MDBCardTitle style={{position:'relative',left:'40px',bottom:'-10px'}}>React Todo App</MDBCardTitle>
+              <MDBCardText><a className="project1-link" href="https://github.com/FaisalQureshi200/React-ToDoApp" target="_blank">View on GitHub</a></MDBCardText>
+              <MDBCardFooter className="project1-footer"/>
+              </MDBCardBody>
+            </MDBCard>
+       
+        
       </Fade>
 
       <br/><br/><br/>
       <Fade>
-        <ExpansionPanel>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography style={{fontSize:'20px',color:'green'}} className={classes.heading}><b><a className="a-1" href="https://github.com/FaisalQureshi200/React-ToDoApp" target="_blank">React Calculator App</a></b></Typography>
-         
-        </ExpansionPanelSummary>
-        <hr/>
-        <ExpansionPanelDetails>
-          <Typography>
-            <ul>
-                <li><b>Front End App</b></li>
-                <li><b>Perform Basic Mathmatics Operation</b>  </li>
-              
-            </ul>
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
+      <MDBCard className="project-card2">
+              <MDBCardBody>
+                <img className="project2-img" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt="react1"/>
+                <MDBCardTitle style={{position:'relative',left:'40px',bottom:'-10px'}}>React Calculator</MDBCardTitle>
+              <MDBCardText><a className="project2-link" href="https://github.com/FaisalQureshi200/ReactProject" target="_blank">View on GitHub</a></MDBCardText>
+              <MDBCardFooter className="project2-footer"/> 
+              </MDBCardBody>
+            </MDBCard>
       </Fade>
-      </div>
+     
       
             </Layout>
             </div>
